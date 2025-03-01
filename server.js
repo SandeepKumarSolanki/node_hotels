e0512add679e488671c08022c38834326c223e36
SandeepKumar <sandeepkusolanki@gamil.com>
2025-03-02 04:27:23 +0530

    mongoDB-changes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //req.body
 
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -25,6 +27,6 @@ app.use('/menu', menuItemRoutes);
 
 
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
